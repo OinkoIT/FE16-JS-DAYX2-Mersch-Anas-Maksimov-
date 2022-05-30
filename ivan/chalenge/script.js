0679a6213e5c78757c1713e08fbe230025a65653
Ivan Maksimov <ivan_kratovo@hotmail.com>
2022-05-30 14:40:04 +0200

    a

---
+++ ivan/chalenge/script.js
@@ -0,0 +1,16 @@
+
+
+
+// We discussed previously a couple of examples (Algorithms & Data Structures)
+
+// Go back to the material and try to program in JavaScript: 
+
+// - ATM machine (if you had more time. try to create a nice layout)
+
+
+let banknotes = [[500,10], [100,20], [50 ,30] , [20,40], [10,50], [5, 60] ];
+
+// to be continued ...
+
+
+
