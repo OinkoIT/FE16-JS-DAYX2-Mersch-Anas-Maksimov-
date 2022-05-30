34e9007a238121c6ed2802c33bd844079dc98777
OinkoIT <claus.mersch@gmail.com>
2022-05-30 11:29:15 +0200

    intermediate

--- claus/02_intermediate/script.js
+++ claus/02_intermediate/script.js
@@ -5,6 +5,28 @@ E.g. If we pass the following string 'i am a web developer' we should get the me
 
 Hint: Check JavaScript String charAt() Method*/
 
-function {
+// var string = ("hello world.")
 
-}();
+(function () {
+    let string = ("hello world.")
+    let firstLetter =  string[0].toUpperCase();
+    console.log(firstLetter + string.slice(1));
+   }());
+
+/*Average Grade
+Make a function that will accept 3 parameters, which are the grades from Math, Physics and English. then make the calculation and print them on the screen. Make sure that the variables are numbers.
+
+E.g. If you put the following grades 3, 4, 5 the result should be:
+
+Sum:12
+
+Average: 4*/
+
+function grades(math, physics, english) {
+    let sum = math + physics + english;
+    let average = sum / 3;
+    console.log("Sum: " + sum)
+    console.log("Average: " + average)
+}
+
+grades(1,2,3);
